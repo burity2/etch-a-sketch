5fc3c4b44b6e10b86496891f66cee91dfd2136c3
burity2 <arthur.rosa66@gmail.com>
2025-04-23 01:19:08 -0300

    fixed the button that clears the canvas!

--- script.js
+++ script.js
@@ -3,11 +3,22 @@
 for (i=0; i < 16; i++) {
     const grid = document.createElement("div");
     grid.className = "grid";
+
     document.getElementById("container").appendChild(grid);
     grid.addEventListener("mouseover", () => {
         grid.style.backgroundColor = "red"
     })
 }
+
+//button that clear canvas
+const canvasClear = document.getElementById("clear-canvas")
+canvasClear.addEventListener("click", () => {
+    console.log("at least the click works")
+    const canvaSelector = document.querySelectorAll(".grid")
+    for (j=0; j < canvaSelector.length; j++) {
+        canvaSelector[j].style.backgroundColor = "#dad6c0";
+    }
+})
 
 // button that resize current canvas
 const canvasResize = document.getElementById("resize-canvas")
@@ -15,10 +26,7 @@ canvasResize.addEventListener("click", () =>{
     prompt("insert canvas dimensions:")  
 })
 
-```
-const hover = document.querySelector(".grid");
-hover.addEventListener("mouseover", () => {
-    alert("Did this work?")
-})
-```
+// button that randomizes the sketch color
+
+// button that activates opacity mode
 
